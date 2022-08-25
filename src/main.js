d345ad8668f692dfcebde5ddbb3490e47861744f
fhp <bliblibli@live.cn>
2022-08-25 22:53:25 +0800

    测试提交

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
+/*
+ * @Author: FHP
+ * @Description: 哈哈
+ * @Version: 1.0
+ * @Date: 2022-08-25 22:51:29
+ * @LastEditors: FHP
+ * @LastEditTime: 2022-08-25 22:52:13
+ * @FilePath: \common\src\main.js
+ * Copyright (C) 2022 FHP. All rights reserved.
+ */
 import Vue from 'vue'
 import App from './App.vue'
-
+// 我来提交一次
 Vue.config.productionTip = false
 
 new Vue({
